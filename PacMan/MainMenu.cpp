4a69c0e05e2e74fe5179c02970307d1a877f8fa2
CosminIlies <cosminilies1234@gmail.com>
2024-06-02 20:56:44 +0300

    qwe

--- PacMan/MainMenu.cpp
+++ PacMan/MainMenu.cpp
@@ -9,24 +9,24 @@ void MainMenu::init()
 				"./res/BoogieNightsShadowNF.ttf",
 				128,
 				sf::Color::White  ,
-				sf::Vector2f(800, 200),
+				sf::Vector2f(800, 150),
 				0, sf::Vector2f(2, 2));	
 	
 	Button* playBtn = new Button("Play",
 		[]() { SceneManagerInstance->nextScene(); return true; },
 		new Sprite("./res/buttonBg.png"),
-		sf::Vector2f(800,500),
+		sf::Vector2f(800,450),
 		0, 
 		sf::Vector2f(15, 15));
 
 	Button* quitBtn = new Button("Quit",
 		[]() {  ApplicationInstance->stop();  return true;  },
 		new Sprite("./res/buttonBg.png"),
-		sf::Vector2f(800, 700),
+		sf::Vector2f(800, 650),
 		0,
 		sf::Vector2f(15, 15));
 
-	Text* proj = new Text("This application is part of the following EU project: \"Gender, Digitalization, Green: Ensuring a Sustainable Future for all in Europe\" Ref. Project: 2023-1-RO01- KA220-HED-000154433", "./res/m6x11plus.ttf", 20, sf::Color::White, sf::Vector2f(800, 850), 0, sf::Vector2f(1,1));
+	Text* proj = new Text("EU Programme Erasmus+\n Project title : \"Gender, Digitalization, Green: Ensuring a Sustainable Future for all in Europe\"\nProject No : 2023 - 1 - RO01 - KA220 - HED - 000154433\nStudents :\nIlies Eduard Cosmin, eduard_cosmin.ilies@stud.fiir.upb.ro\nGeorgescu Gabriel, gabriel.georgescu04@stud.fiir.upb.ro\nChelu Fabian Catalin, fabian.chelu@stud.fiir.upb.ro\nDaescu Cristian - Narcis, cristian.daescu@stud.fiir.upb.ro\nTeachers : Prof M.Caramihai& D.Chis", "./res/m6x11plus.ttf", 16, sf::Color::White, sf::Vector2f(800, 800), 0, sf::Vector2f(1,1));
 
 
 	//addEntity(bg);
